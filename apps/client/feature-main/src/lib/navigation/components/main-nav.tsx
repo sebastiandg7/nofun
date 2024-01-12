75612f43166538b3ae811d5f49e265b38075cc3c
Sebastian Duque Gutierrez <13395979+sebastiandg7@users.noreply.github.com>
2024-01-12 00:26:01 -0500

    main nav items

--- apps/client/feature-main/src/lib/navigation/components/main-nav.tsx
+++ apps/client/feature-main/src/lib/navigation/components/main-nav.tsx
@@ -1,12 +1,102 @@
 import { siteConfig } from '@nofun/client-util-config';
 import { cn } from '@nofun/tailwind-util-class-names';
 import { Icons } from '@nofun/ui-components';
-import { NavLink, useLocation } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@nofun/ui-components';
+import React from 'react';
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: 'Spy',
+    href: '/spy',
+    description:
+      'A modal dialog that interrupts the user with important content and expects a response.',
+  },
+  {
+    title: 'Papelito',
+    href: '/papelito',
+    description:
+      'For sighted users to preview content available behind a link.',
+  },
+  {
+    title: 'Dice',
+    href: '/dice',
+    description:
+      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
+  },
+];
+
+export function NavigationMenuDemo() {
+  const { pathname } = useLocation();
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link to="/">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Home
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        {pathname !== '/' && (
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Games</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {components.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    to={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        )}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+const ListItem = React.forwardRef<
+  HTMLAnchorElement,
+  React.ComponentPropsWithoutRef<typeof Link>
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <Link
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = 'ListItem';
 
 export function MainNav() {
-  const location = useLocation();
-  const { pathname } = location;
-
   return (
     <div className="mr-4 hidden md:flex">
       <NavLink to="/" className="mr-6 flex items-center space-x-2">
@@ -16,28 +106,7 @@ export function MainNav() {
         </span>
       </NavLink>
       <nav className="flex items-center space-x-6 text-sm font-medium">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            cn(
-              'transition-colors hover:text-foreground/80',
-              isActive ? 'text-foreground' : 'text-foreground/60'
-            )
-          }
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/settings"
-          className={cn(
-            'transition-colors hover:text-foreground/80',
-            pathname?.startsWith('/settings')
-              ? 'text-foreground'
-              : 'text-foreground/60'
-          )}
-        >
-          Settings
-        </NavLink>
+        <NavigationMenuDemo />
       </nav>
     </div>
   );
