b157f54d813a327263eede117779a48acda035ba
Sebastian Duque Gutierrez <13395979+sebastiandg7@users.noreply.github.com>
2024-01-18 19:32:52 -0500

    update ga id

--- apps/client/app/src/main.tsx
+++ apps/client/app/src/main.tsx
@@ -4,11 +4,13 @@ import './i18n/i18n';
 import App from './app/app';
 import ReactGA from 'react-ga4';
 
-ReactGA.initialize([
-  {
-    trackingId: 'G-VVCQWLGPGD',
-  },
-]);
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize([
+    {
+      trackingId: 'G-SVXF88W9N5',
+    },
+  ]);
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
