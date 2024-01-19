8467bdf18443790af7ac900830b3acc5035c1d47
Sebastian Duque Gutierrez <13395979+sebastiandg7@users.noreply.github.com>
2024-01-18 19:01:27 -0500

    ga setup

--- apps/client/app/src/main.tsx
+++ apps/client/app/src/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './i18n/i18n';
 import App from './app/app';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize([
+  {
+    trackingId: 'G-VVCQWLGPGD',
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- apps/client/feature-main/src/lib/main.tsx
+++ apps/client/feature-main/src/lib/main.tsx
@@ -1,11 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { SiteHeader } from './components/site-header/site-header';
 import { ReloadPrompt } from './components/reload-prompt/reload-prompt';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 
 /* eslint-disable-next-line */
 export interface MainProps {}
 
 export function Main(props: MainProps) {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.send({
+      hitType: 'pageview',
+      page: location.pathname + location.search,
+      title: location.key,
+    });
+  }, [location]);
+
   return (
     <main className="min-h-screen flex flex-col">
       <ReloadPrompt />
