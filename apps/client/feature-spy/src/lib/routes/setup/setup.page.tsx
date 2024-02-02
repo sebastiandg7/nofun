9123d341e4444d0860aaeb9a16bc0488661a601f
Sebastian Duque Gutierrez <13395979+sebastiandg7@users.noreply.github.com>
2024-02-02 15:38:50 -0500

    spy: help dialog (#19)
    * adding stepper component
    
    * changing stepper colors
    
    * adding help dialog to setup page
    
    * feat(spy): adding help dialog
    
    * fix(format): formatting code
    
    * feat(spy/setup): adding reset default configs
    
    ---------
    
    Co-authored-by: Andres Valencia <andres.valencia@skuid.com>

--- apps/client/feature-spy/src/lib/routes/setup/setup.page.tsx
+++ apps/client/feature-spy/src/lib/routes/setup/setup.page.tsx
@@ -4,9 +4,16 @@ import { useTranslation } from 'react-i18next';
 import { GameStage, spyGameSessionAtom } from '../../+state/game-session.state';
 import { SPY_NAMESPACE } from '../../i18n/constants';
 import { GameSettings } from './components/game-settings';
+import { RotateCcw } from 'lucide-react';
+import { HelpDialog } from './components/help-dialog';
+import {
+  defaultGameSettings,
+  spyGameSettingsAtom,
+} from '../../+state/game-settings.state';
 
 export function SetupPage() {
   const [, setGameSession] = useAtom(spyGameSessionAtom);
+  const [, setGameSettings] = useAtom(spyGameSettingsAtom);
   const { t } = useTranslation(SPY_NAMESPACE);
 
   function onStartGamePressed() {
@@ -17,13 +24,26 @@ export function SetupPage() {
     });
   }
 
+  function resetConfig() {
+    setGameSettings(defaultGameSettings);
+  }
+
   return (
-    <div className="flex-1 flex flex-col justify-center h-full">
+    <div className="flex-1 flex flex-col justify-center h-full relative mx-auto">
+      <HelpDialog />
       <h1 className="text-xl font-bold text-gray-900 dark:text-gray-100 mx-auto">
-        {t('Configura tu juego')}
+        {t('Configuración')}
       </h1>
+      <Button
+        size="icon"
+        variant="secondary"
+        className="ml-5 mr-auto"
+        onClick={resetConfig}
+      >
+        <RotateCcw className="w-4 h-4" />
+      </Button>
       <GameSettings />
-      <Button className="mx-auto min-w-32" onClick={onStartGamePressed}>
+      <Button className="mt-2 mx-auto min-w-32" onClick={onStartGamePressed}>
         Jugar!
       </Button>
     </div>
