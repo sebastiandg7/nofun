6ffe78ad30a6b55de5878b58307452463bc53047
Andres Valencia <andres.valencia@skuid.com>
2024-02-02 15:31:23 -0500

    feat(spy/setup): adding reset default configs

--- apps/client/feature-spy/src/lib/routes/setup/setup.page.tsx
+++ apps/client/feature-spy/src/lib/routes/setup/setup.page.tsx
@@ -6,9 +6,14 @@ import { SPY_NAMESPACE } from '../../i18n/constants';
 import { GameSettings } from './components/game-settings';
 import { RotateCcw } from 'lucide-react';
 import { HelpDialog } from './components/help-dialog';
+import {
+  defaultGameSettings,
+  spyGameSettingsAtom,
+} from '../../+state/game-settings.state';
 
 export function SetupPage() {
   const [, setGameSession] = useAtom(spyGameSessionAtom);
+  const [, setGameSettings] = useAtom(spyGameSettingsAtom);
   const { t } = useTranslation(SPY_NAMESPACE);
 
   function onStartGamePressed() {
@@ -19,14 +24,23 @@ export function SetupPage() {
     });
   }
 
+  function resetConfig() {
+    setGameSettings(defaultGameSettings);
+  }
+
   return (
     <div className="flex-1 flex flex-col justify-center h-full relative mx-auto">
       <HelpDialog />
       <h1 className="text-xl font-bold text-gray-900 dark:text-gray-100 mx-auto">
         {t('Configuración')}
       </h1>
-      <Button size="icon" variant="secondary" className="ml-5 mr-auto" onClick={onStartGamePressed}>
-        <RotateCcw className='w-4 h-4'/>
+      <Button
+        size="icon"
+        variant="secondary"
+        className="ml-5 mr-auto"
+        onClick={resetConfig}
+      >
+        <RotateCcw className="w-4 h-4" />
       </Button>
       <GameSettings />
       <Button className="mt-2 mx-auto min-w-32" onClick={onStartGamePressed}>
