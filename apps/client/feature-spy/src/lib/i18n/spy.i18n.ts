6a8b307899baa8dcfd4af920c8ff08eff7069b7a
Andres Valencia <andres.valencia@skuid.com>
2024-02-07 12:59:53 -0500

    feat(spy.i18n): adding setup translations

--- apps/client/feature-spy/src/lib/i18n/spy.i18n.ts
+++ apps/client/feature-spy/src/lib/i18n/spy.i18n.ts
@@ -7,6 +7,8 @@ interface SpyTranslations {
     description: string;
   };
   setup: {
+    title: string;
+    play_btn: string;
     help: {
       title: string;
       button: {
@@ -19,7 +21,36 @@ interface SpyTranslations {
       step3: string;
       step4: string;
     };
+    settings: {
+      ready_btn: string;
+      players: {
+        title: string;
+        num: string;
+      }
+      spies: {
+        title: string;
+        description: string;
+        num: string;
+      }
+      categories: {
+        title: string;
+        description: string;
+        toggle_all: string;
+      }
+      timer: {
+        title: string;
+        description: string;
+        toggle: string;
+        time: string;
+        deactivate: string;
+      }
+    }
   };
+  board: {
+    spy_player: string;
+    player: string;
+    card_hide: string;
+  }
 }
 
 export const spyTranslations = buildI18NResource<SpyTranslations>({
@@ -31,6 +62,8 @@ export const spyTranslations = buildI18NResource<SpyTranslations>({
         description: 'Spy is a game where you have to guess the word',
       },
       setup: {
+        title: 'Setup',
+        play_btn: 'Play',
         help: {
           title: 'How to play?',
           button: {
@@ -47,7 +80,36 @@ export const spyTranslations = buildI18NResource<SpyTranslations>({
           step4:
             'Time to find out who the impostor is, ask questions with yes or no answers, and try to uncover the liar.',
         },
+        settings: {
+          ready_btn: 'Ready!',
+          players: {
+            title: 'Players',
+            num: 'Number of players',
+          },
+          spies: {
+            title: 'Spies',
+            num: 'Number of spies',
+            description: 'A maximum of 1 spy per 3 players is recommended',
+          },
+          categories: {
+            title: 'Categories',
+            description: 'Select the categories you want to include in the game',
+            toggle_all: 'All'
+          },
+          timer: {
+            title: 'Time',
+            description: 'How much time to uncover the spies?',
+            time: 'Minutes',
+            toggle: 'Use timer',
+            deactivate: 'Deactivated'
+          }
+        }
       },
+      board: {
+        spy_player: 'Spy',
+        player: 'Player',
+        card_hide: 'Tap to hide the card and move on to the next player.'
+      }
     },
     es: {
       game: {
@@ -55,6 +117,8 @@ export const spyTranslations = buildI18NResource<SpyTranslations>({
         description: 'Espía es un juego donde tienes que adivinar la palabra',
       },
       setup: {
+        title: 'Configuracion',
+        play_btn: 'Jugar',
         help: {
           title: '¿Cómo jugar?',
           button: {
@@ -71,7 +135,36 @@ export const spyTranslations = buildI18NResource<SpyTranslations>({
           step4:
             'Hora de descubrir quien es el impostor, hagan preguntas cuyas respuestas sean si o no y traten de descubrir quién miente.',
         },
+        settings: {
+          ready_btn: 'Listo!',
+          players: {
+            title: 'Jugadores',
+            num: 'Cantidad de jugadores',
+          },
+          spies: {
+            title: 'Espías',
+            num: 'Cantidad de espías',
+            description: 'Un máximo de 1 espía por cada 3 jugadores es recomendado',
+          },
+          categories: {
+            title: 'Categorías',
+            description: 'Selecciona qué categorias incluir en el juego',
+            toggle_all: 'Todas'
+          },
+          timer: {
+            title: 'Tiempo',
+            description: 'Cuánto tiempo tendrán para descubrir a los espías?',
+            time: 'Minutos',
+            toggle: 'Usar temporizador',
+            deactivate: 'Desactivado'
+          }
+        }
       },
+      board: {
+        spy_player: 'Espía',
+        player: 'Jugador',
+        card_hide: 'Toca para ocultar la tarjeta y pasa al siguiente jugador'
+      }
     },
   },
 });
