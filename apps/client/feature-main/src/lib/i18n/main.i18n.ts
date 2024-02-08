03b3e221e4cf08ccc7f6f98ca185a147e3512ef8
Sebastian Duque Gutierrez <13395979+sebastiandg7@users.noreply.github.com>
2024-02-07 23:56:11 -0500

    share button

--- apps/client/feature-main/src/lib/i18n/main.i18n.ts
+++ apps/client/feature-main/src/lib/i18n/main.i18n.ts
@@ -4,6 +4,21 @@ import { MAIN_NAMESPACE } from './constants';
 interface MainTranslations {
   title: string;
   description: string;
+  share: { label: string; title: string; text: string };
+  games: {
+    spy: {
+      title: string;
+      description: string;
+    };
+    papelito: {
+      title: string;
+      description: string;
+    };
+    dice: {
+      title: string;
+      description: string;
+    };
+  };
 }
 
 export const mainTranslations = buildI18NResource<MainTranslations>({
@@ -12,10 +27,49 @@ export const mainTranslations = buildI18NResource<MainTranslations>({
     en: {
       title: 'Nofun Games',
       description: 'Choose a game to start the fun!',
+      share: {
+        label: 'Share with friends',
+        title: 'Share',
+        text: 'Join me in Nofun Games!',
+      },
+      games: {
+        spy: {
+          title: 'Spy',
+          description: "Discover the impostor before it's too late.",
+        },
+        papelito: {
+          title: 'Papelito',
+          description: 'Make your team guess as many words as possible.',
+        },
+        dice: {
+          title: 'Dice',
+          description: 'Roll the dice and take your chances.',
+        },
+      },
     },
     es: {
       title: 'Juegos Nofun',
       description: 'Escoge un juego para que comience la diversión!',
+      share: {
+        label: 'Compartir con amigos',
+        title: 'Compartir',
+        text: '¡Juguemos juntos en Nofun Games!',
+      },
+      games: {
+        spy: {
+          title: 'Espía',
+          description: 'Descubre al impostor antes de que sea demasiado tarde.',
+        },
+        papelito: {
+          title: 'Papelito',
+          description:
+            'Haz que tu equipo adivine tantas palabras como sea posible.',
+        },
+        dice: {
+          title: 'Dados',
+          description: 'Tira los dados y toma tus oportunidades.',
+        },
+      },
     },
   },
 });
